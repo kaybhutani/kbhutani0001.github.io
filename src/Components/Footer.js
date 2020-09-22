b84d257dab32b73d75c577ad78b374d970d2d7e8
Kartikay Bhutani <kbhutani0001@gmail.com>
2020-09-22 20:58:02 +0530

    update

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -19,7 +19,7 @@ class Footer extends Component {
            </ul>
 
            <ul>
-            <li>Modified with love by the Clever Programmer team ♥️</li>
+            <li>Made with ♥️ by Kartikay Bhutani</li>
            </ul>
 
            <ul className="copyright">
