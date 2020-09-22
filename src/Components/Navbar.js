a5ed8335fda239069a18a4ac1d31b04ed6d79135
Kartikay Bhutani <kbhutani0001@gmail.com>
2020-09-22 19:49:45 +0530

    basic structure fork

---
+++ src/Components/Navbar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <div>
+      <nav style={{display:"inline"}}>
+        <li>About</li>
+        <li>Experience</li>
+        <li>Skills</li>
+        <li>Resume</li>
+      </nav>
+    </div>
+  )
+}
+
+export default Navbar;
